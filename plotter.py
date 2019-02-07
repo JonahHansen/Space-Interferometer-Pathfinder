73ecc4e177ed16dfc233060eded39ec01cc22368
JonahHansen <hansen.jonah833@gmail.com>
2019-02-07 19:09:44 +1100

    Cleanup

--- plotter.py
+++ plotter.py
@@ -3,7 +3,7 @@ A script to plot orbits simply for illustration
 
 Note that cartopy and Basemap both should work here.
 
-1 Feb 2019: Initial version by Mike Ireland. Still has a bug.
+7 Feb 2019: Version 2.0 by Jonah Hansen. Works for circular orbits.
 """
 from __future__ import print_function
 import matplotlib.pyplot as plt
@@ -11,6 +11,7 @@ import numpy as np
 import astropy.constants as const
 import astropy.units as u
 from mpl_toolkits.basemap import Basemap
+import quaternions as qt
 plt.ion()
 
 alt = 500.0e3   #In m
@@ -18,19 +19,21 @@ R_e = 6375.0e3  #In m
 n_p = 1000 #Number of phases
 
 #Orbital inclination
-inc0 = np.radians(49)
-phi0 = np.radians(-30)
+inc_0 = np.radians(24) #49
+#Longitude of the Ascending Node
+Om_0 = np.radians(-24) #-30
 
 #Stellar vector
-ra = np.radians(23)
-dec = np.radians(43)
+ra = np.radians(0) #23
+dec = np.radians(43)#43
 
 #The distance to the other satellites in km
-b = 640e3
+b = 350e3
 
 period = 96.0 #In minutes.
 lines = ['r:', 'g:', 'g:']
 points = ['r.', 'g.', 'g.']
+
 #------------------------------------------------------------------------------------------
 #Orbital radius the sum of earth radius and altitude
 R_orb = R_e + alt
@@ -46,59 +49,60 @@ for i in range(n_p):
 #Cartesian coordinates for all 3 spacecraft
 xyzo = np.zeros( (3,n_p,3) )
 
-#Rotation matrix and orbital angular momentum vector for central combiner.
-#Create this matrix by rotating in azimuthal angle phi (longitude of the ascending (?)
-#node) then rotating in inclination,
-#called both th and inc below. Probably use conventional terms i and Omega!
+#Initial axis unit vectors
+xaxis = np.array([1,0,0])
+yaxis = np.array([0,1,0])
+zaxis = np.array([0,0,1])
 
-R_phi = np.array([[np.cos(phi0), -np.sin(phi0),0],[np.sin(phi0), np.cos(phi0), 0],[0,0,1]])
-R_th = np.array([[np.cos(inc0),0,np.sin(inc0)], [0, 1, 0], [-np.sin(inc0), 0, np.cos(inc0)]])
-R_0 = R_phi.dot(R_th).dot(np.linalg.inv(R_phi))
-o_0 = R_0[:,2]
+#Quaternion rotation, using Mike's "No phase difference" rotation!
+q_Om = qt.to_q(zaxis,Om_0)
+q_inc = qt.to_q(yaxis,inc_0)
+q_0 = qt.comb_rot(qt.comb_rot(qt.conjugate(q_Om),q_inc),q_Om)
+
+#Cartesian points on the rotated orbit
+xyzo[0] = qt.rotate_points(xyzc,q_0)
+
+#Angular momentum vector of chief satellite
+h_0 = qt.rotate(zaxis,q_0)
 
 #Vector pointing to the star, from right ascension and declination
 s_hat = [np.cos(ra)*np.cos(dec), np.sin(ra)*np.cos(dec), np.sin(dec)]
 
-#Now lets find the other satellites' phi and inc. Mike thought it would be cool to
-#use vector arithmetic for this.
-epsilon = b/R_orb
-delta = epsilon #* np.dot(o_0, s_hat)
-o_1 = o_0 + epsilon*np.cross(o_0,s_hat)
-o_2 = o_0 - epsilon*np.cross(o_0,s_hat)
-o_1 /= np.linalg.norm(o_1)
-o_2 /= np.linalg.norm(o_2)
-inc_1 = np.arccos(o_1[2])
-phi_1= np.arctan2(o_1[1], o_1[0])
-#phi_1= -np.arctan2(o_1[0], o_1[1])
-inc_2 = np.arccos(o_2[2])
-phi_2= np.arctan2(o_2[1], o_2[0])
-#phi_2= -np.arctan2(o_2[0], o_2[1])
+#New coord system:
+z_hat = h_0/np.linalg.norm(h_0) #In direction of angular momentum
+y = s_hat-z_hat*(np.dot(s_hat,z_hat)) #Projection of the star vector on the orbital plane
+y_hat = y/np.linalg.norm(y)
+x_hat = np.cross(y_hat,z_hat) #Remaining orthogonal vector
 
-#Next, compute the orbital rotation matrices for these two other satellites.
-Rs = [R_0]
-poffsets = [0,-delta, delta]
-print(phi0, inc0)
-for phi, inc in zip([phi_1, phi_2], [inc_1, inc_2]):
-    print(phi, inc)
-    R_phi = np.array([[np.cos(phi), -np.sin(phi),0],[np.sin(phi), np.cos(phi), 0],[0,0,1]])
-    R_th = np.array([[np.cos(inc),0,np.sin(inc)], [0, 1, 0], [-np.sin(inc), 0, np.cos(inc)]])
-    Rs.append(R_phi.dot(R_th).dot(np.linalg.inv(R_phi)))
+#Angle between angular momentum vector and star:
+theta =np.arccos(np.dot(z_hat,s_hat))
 
-#Use the Rotation matrices to find the orbital positions.
-for ix, R in enumerate(Rs):
-    for iy in range(n_p):
-        xyzo[ix,iy] = np.dot(R, xyzc[iy])
+psi = b/R_orb #Angle between chief and deputy WRT Earth
+
+#Define deputy orbital planes in terms of a rotation of the chief satellite
+axis1 = np.cos(psi)*y_hat - np.sin(psi)*x_hat #Axis of rotation
+angle1 = psi*np.tan(theta) #Amount of rotation
+q_phase1 = qt.to_q(z_hat,psi) #Rotate in phase
+q_plane1 = qt.to_q(axis1,angle1) #Rotate around axis
+q_orb1 = qt.comb_rot(q_phase1,q_plane1) #Combine
+
+#Same as above but for the second deputy
+axis2 = np.cos(-psi)*y_hat - np.sin(-psi)*x_hat
+angle2 = -psi*np.tan(theta)
+q_phase2 = qt.to_q(z_hat,-psi)
+q_plane2 = qt.to_q(axis2,angle2)
+q_orb2 = qt.comb_rot(q_phase2,q_plane2)
+
+#Rotate the chiefs orbit
+xyzo[1] = qt.rotate_points(xyzo[0],q_orb1)
+xyzo[2] = qt.rotate_points(xyzo[0],q_orb2)
 
 #Lets compute dot products for the full orbit
-
-for ix in range(n_p):
+for ix in range(0,n_p,10):
     sep1 = xyzo[1,ix] - xyzo[0,ix]
     sep2 = xyzo[2,ix] - xyzo[0,ix]
-    sep1 /= np.linalg.norm(sep1)
-    sep2 /= np.linalg.norm(sep2)
-    print(sep1,sep2,s_hat)
-    print("Dot products with star vector: {:6.1f} {:6.1f}".format(np.dot(s_hat, sep1), np.dot(s_hat, sep2)))
-
+    print(np.dot(sep1,s_hat),np.dot(sep2,s_hat))
+    print("Angles to correct plane: {:6.3f} {:6.3f}".format(np.arcsin(np.dot(s_hat, sep1/np.linalg.norm(sep1))), np.arcsin(np.dot(s_hat, sep2/np.linalg.norm(sep2)))))
 
 #Make pretty plots.
 for im_ix, sat_phase in enumerate(np.linspace(np.pi,2.*np.pi,6)): #np.pi, 31*np.pi,450))
@@ -108,7 +112,7 @@ for im_ix, sat_phase in enumerate(np.linspace(np.pi,2.*np.pi,6)): #np.pi, 31*np.
     map.bluemarble(scale=0.4)
     plt.axis([-0.1*R_orb, 2.1*R_orb, -0.0*R_orb, 2.0*R_orb])
     #Find non-vignetted parts (two vectors)
-    for xyz, poffset, point, line in zip(xyzo, poffsets, points, lines):
+    for xyz, point, line in zip(xyzo, points, lines):
         visible = (xyz[:,1] > 0) | (np.sqrt(xyz[:,0]**2 + xyz[:,2]**2) > R_e)
         visible = np.concatenate(([False],visible, [False]))
         out_of_eclipse = np.where(visible[1:] & np.logical_not(visible[:-1]))[0]
@@ -117,11 +121,11 @@ for im_ix, sat_phase in enumerate(np.linspace(np.pi,2.*np.pi,6)): #np.pi, 31*np.
             plt.plot(xyz[oute:ine+1,0] + R_e, xyz[oute:ine+1,2] + R_e,line)
 
         #Interpolate to current time.
-        sat_xyz = [np.interp( (sat_phase-poffset) % (2*np.pi), phase, xyz[:,ii]) for ii in range(3)]
+        sat_xyz = [np.interp( (sat_phase) % (2*np.pi), phase, xyz[:,ii]) for ii in range(3)]
 
         #If in foreground or more than R_earth away in (x,z) plane, plot.
         if (sat_xyz[1] > 0) | (np.sqrt(sat_xyz[0]**2 + sat_xyz[2]**2) > R_e):
             plt.plot(sat_xyz[0] + R_e, sat_xyz[2] + R_e,point)
     plt.tight_layout()
     #plt.savefig("pngs/orb{:03d}.png".format(im_ix))
-    plt.pause(.1)
+    plt.pause(.05)
