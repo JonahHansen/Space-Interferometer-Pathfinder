b1b0554414fbdc21943e3103cf6f66d9fc9b1ab2
JonahHansen <hansen.jonah833@gmail.com>
2019-02-13 12:56:05 +1100

    Added perturbations

---
+++ perturbations.py
@@ -0,0 +1,57 @@
+#Perturbations
+
+import numpy as np
+from astropy import units as unit
+from poliastro.core.perturbations import atmospheric_drag, third_body, J2_perturbation, radiation_pressure
+from poliastro.bodies import Earth
+
+def perturbations(t0,u,k,index_ls,**kwargs):
+    
+    final_accel = 0
+    R_E=Earth.R.to(unit.km).value
+    
+    ### J2 = 1 ###
+    if 1 in index_ls:
+        
+        J2_acc = J2_perturbation(t0,u,k,Earth.J2.value,R_E)
+        final_accel += J2_acc
+        
+        
+    ### Drag = 2 ###
+    if 2 in index_ls:
+    
+        #C_D (float) – dimensionless drag coefficient ()
+        #A (float) – frontal area of the spacecraft (km^2)
+        #m (float) – mass of the spacecraft (kg)
+        #H0 (float) – atmospheric scale height (km)
+        #rho0 (float) – the exponent density pre-factor (kg / m^3)
+        
+        drag = atmospheric_drag(t0,u,k,R_E,kwargs["C_D"],kwargs["A1"],kwargs["m"],kwargs["H0"],kwargs["rho0"])
+        final_accel += drag
+        
+        
+    ### Third Body = 3 ###
+    if 3 in index_ls:
+        
+        #k (float) – gravitational constant of third body (km^3/s^2)
+        #third_body (a callable object returning the position of 3rd body) – third body that causes the perturbation
+        
+        t_body = third_body(t0,u,k,kwargs["k_third"],kwargs["third_body"])
+        final_accel += t_body
+        
+        
+    ### Rad_pressure = 4 ###
+    if 4 in index_ls:
+        
+        #C_R (float) – dimensionless radiation pressure coefficient, 1 < C_R < 2 ()
+        #A (float) – effective spacecraft area (km^2)
+        #m (float) – mass of the spacecraft (kg)
+        #Wdivc_s (float) – total star emitted power divided by the speed of light (W * s / km)
+        #star (a callable object returning the position of star in attractor frame) – star position
+        
+        rad_pressure = (t0,u,k,R_E,kwargs["C_R"],kwargs["A2"],kwargs["m"],kwargs["Wdivc_s"],kwargs["star"])
+        final_accel += rad_pressure
+    
+    
+    return final_accel
+    
