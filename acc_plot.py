e5a76adfe2850e9d7ee52139d812ef8af31e9ae1
JonahHansen <hansen.jonah833@gmail.com>
2019-03-05 09:04:16 +1100

    Added max_acc files

--- acc_plot.py
+++ acc_plot.py
@@ -18,7 +18,7 @@ def plotacc(index,array,title,clabel):
     else:
         vmin = 1e-7
         vmax = 1e-2
-        
+
     plt.imshow(result,origin="lower",cmap="viridis",extent=[-90,90,0,90],vmin=vmin,vmax=vmax)
     cbar = plt.colorbar()
     cbar.set_label(clabel, rotation=270, labelpad=15)
@@ -46,9 +46,9 @@ def plot_ave_del_v(idx,array,title):
         result[int(item[0,0])] = sum_del_v/weights
     plt.bar(np.linspace(0,90,len_ix),result)
     plt.xlabel("Inclination (deg)")
-    plt.ylabel("Log Delta_V ()")
+    plt.ylabel("Delta_V (m/s)")
     plt.title(title)
-        
+
 def plot_max_ac(idx,array,title):
     len_ix = np.shape(a)[0]
     len_iy = np.shape(a)[1]
@@ -59,32 +59,31 @@ def plot_max_ac(idx,array,title):
         result[int(item[0,0])] = np.max(item[:,((idx % 10)+2)])
     plt.bar(np.linspace(0,90,len_ix),result)
     plt.xlabel("Inclination (deg)")
-    plt.ylabel("Log Maximum acceleration ()")
+    plt.ylabel("Maximum acceleration (m/s/s)")
     plt.title(title)
-    
-    
-
-a = np.load("2acc_variance_alt1000_ra0.npy")
-b = np.load("2acc_variance_alt500_ra0.npy")
-
-plotacc(0,a,"Altitude 1000km, Deputy 1",'Log maximum accceleration in s direction')
-plotacc(1,a,"Altitude 1000km, Deputy 2",'Log maximum accceleration in s direction')
-plotacc(2,a,"Altitude 1000km, Deputy separation",'Log maximum accceleration in b direction')
-
-plotacc(3,a,"Altitude 1000km, Deputy 1",'Log delta_v in s direction')
-plotacc(4,a,"Altitude 1000km, Deputy 2",'Log delta_v in s direction')
-plotacc(5,a,"Altitude 1000km, Deputy separation",'Log delta_v in b direction')
-
-plotacc(6,b,"Altitude 500km, Deputy 1",'Log maximum accceleration in s direction')
-plotacc(7,b,"Altitude 500km, Deputy 2",'Log maximum accceleration in s direction')
-plotacc(8,b,"Altitude 500km,  Deputy separation",'Log maximum accceleration in b direction')
 
 
-plotacc(9,b,"Altitude 500km, Deputy 1",'Log delta_v in s direction')
-plotacc(10,b,"Altitude 500km, Deputy 2",'Log delta_v in s direction')
-plotacc(11,b,"Altitude 500km,  Deputy separation",'Log delta_v in b direction')
 
-"""
+a = np.load("2acc_variance_alt1000_ra90.npy")
+b = np.load("2acc_variance_alt500_ra90.npy")
+
+plotacc(0,a,"Altitude 1000km, Deputy 1",'Maximum accceleration in s direction (m/s/s)')
+plotacc(1,a,"Altitude 1000km, Deputy 2",'Maximum accceleration in s direction (m/s/s)')
+plotacc(2,a,"Altitude 1000km, Deputy separation",'Maximum accceleration in b direction (m/s/s)')
+
+plotacc(3,a,"Altitude 1000km, Deputy 1",'Delta_v in s direction (m/s)')
+plotacc(4,a,"Altitude 1000km, Deputy 2",'Delta_v in s direction (m/s)')
+plotacc(5,a,"Altitude 1000km, Deputy separation",'Delta_v in b direction (m/s)')
+
+plotacc(6,b,"Altitude 500km, Deputy 1",'Maximum accceleration in s direction (m/s/s)')
+plotacc(7,b,"Altitude 500km, Deputy 2",'Maximum accceleration in s direction (m/s/s)')
+plotacc(8,b,"Altitude 500km,  Deputy separation",'Maximum accceleration in b direction (m/s/s)')
+
+
+plotacc(9,b,"Altitude 500km, Deputy 1",'Delta_v in s direction (m/s)')
+plotacc(10,b,"Altitude 500km, Deputy 2",'Delta_v in s direction (m/s)')
+plotacc(11,b,"Altitude 500km,  Deputy separation",'Delta_v in b direction (m/s)')
+
 print("Max acc 1000")
 plot_max_ac(20,a,"Max Acc Hist: Altitude 1000km, Deputy 1, s direction")
 plot_max_ac(21,a,"Max Acc Hist: Altitude 1000km, Deputy 2, s direction")
@@ -106,7 +105,7 @@ print("delv 500")
 plot_ave_del_v(33,b,"Ave DelV Hist: Altitude 500km, Deputy 1, s direction")
 plot_ave_del_v(34,b,"Ave DelV Hist: Altitude 500km, Deputy 2, s direction")
 plot_ave_del_v(35,b,"Ave DelV Hist: Altitude 500km, Deputy separation, b direction")
-"""
+
 
 
 plt.show()
